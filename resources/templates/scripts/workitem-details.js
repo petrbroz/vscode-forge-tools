446d90d72e1b115cfe67410561e9617872e2b41e
Michal Vasicek <michal.vasicek@autodesk.com>
2020-10-30 22:02:15 +0100

    Adding support for Read verb for activity params. It allows to place arguments value on the activity command line.

--- resources/templates/scripts/workitem-details.js
+++ resources/templates/scripts/workitem-details.js
@@ -6,25 +6,33 @@ $('#start').click(function () {
     $('#parameters > tbody > tr').each(function () {
         const $row = $(this);
         const name = $row.find("input[name='param-name']").val();
-        parameters[name] = {
-            url: $row.find("input[name='param-value']").val(),
-            verb: $row.find("select[name='param-verb']").val(),
-            optional: $row.find("input[name='param-optional']")[0].checked
-        };
-        const localName = $row.find("input[name='param-local']").val();
-        if (localName) {
-            parameters[name].localName = localName;
-        }
-        const pathInZip = $row.find("input[name='param-pathinzip']").val();
-        if (pathInZip) {
-            parameters[name].pathInZip = pathInZip;
-        }
-        const headers = $row.find("input[name='param-headers']").val();
-        if (headers) {
-            parameters[name].headers = {};
-            for (const header of headers.split(';')) {
-                const tokens = header.split(':');
-                parameters[name].headers[tokens[0].trim()] = tokens[1].trim();
+        const verb = $row.find("select[name='param-verb']").val();
+
+        if (verb === 'read') {
+            parameters[name] = {
+                value: $row.find("input[name='param-value']").val(),
+            };
+        } else {
+            parameters[name] = {
+                url: $row.find("input[name='param-value']").val(),
+                verb: verb,
+                optional: $row.find("input[name='param-optional']")[0].checked
+            };
+            const localName = $row.find("input[name='param-local']").val();
+            if (localName) {
+                parameters[name].localName = localName;
+            }
+            const pathInZip = $row.find("input[name='param-pathinzip']").val();
+            if (pathInZip) {
+                parameters[name].pathInZip = pathInZip;
+            }
+            const headers = $row.find("input[name='param-headers']").val();
+            if (headers) {
+                parameters[name].headers = {};
+                for (const header of headers.split(';')) {
+                    const tokens = header.split(':');
+                    parameters[name].headers[tokens[0].trim()] = tokens[1].trim();
+                }
             }
         }
     });
